f4543f89decb79760288d8550439cee1ba4a7715
teboum <boumazzat@gmail.com>
2023-10-26 20:19:32 +0100

    second commit

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const helmet = require("helmet");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 
 var cors = require("cors");
@@ -32,11 +37,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 // routes
 app.get("/", (req, res) => {
-    res.send("jobs api");
+    res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>");
 });
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
